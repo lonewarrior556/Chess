e8906e39202068e3260a66e50e55ffcd4d11b914
Alex & Konrad <Alex_Konrad@appacademy.io>
2015-03-31 13:40:57 -0400

    Add Pawn moves

--- piece.rb
+++ piece.rb
@@ -1,5 +1,5 @@
 class Pieces
-  attr_accessor :position
+  attr_accessor :position, :color
 
   def initialize (position, color, board)
     @position = position
@@ -44,6 +44,7 @@ class Sliding_Pieces < Pieces
       pos_moves << [position.first, idx]
       pos_moves << [idx, position.last]
     end
+
     remove_invalid(pos)
   end
 
@@ -54,6 +55,7 @@ class Sliding_Pieces < Pieces
         pos << [position.first + move.first*idx , position.last + move.last*idx]
       end
     end
+
     remove_invalid(pos)
   end
 
@@ -62,17 +64,61 @@ end
 class Stepping_Pieces < Pieces
 
     KNIGHT_MOTION= [ [1,2],[2,1],[1,-2],[-2,1],[-1,2],[2,-1],[-1,-2],[-2,-1]]
+    KING_MOTION=[[0,1],[0,-1],[1,0],[1,1],[1,-1],[-1,-1],[-1,0],[-1,1]]
 
+  def knight_moves
+      pos=[]
+      KNIGHT_MOTION.each do |move|
+        pos << [position.first + move.first, position.last + move.last]
+      end
 
-def knight_moves
-    pos=[]
-    KNIGHT_MOTION.each do |move|
-      pos << [position.first + move.first, position.last + move.last]
-    end
-    remove_invalid(pos)
+      remove_invalid(pos)
+  end
+
+  def king_moves
+      pos=[]
+      KING_MOTION.each do |move|
+        pos << [position.first + move.first, position.last + move.last]
+      end
+
+      remove_invalid(pos)
+  end
+
 end
 
 class Pawns < Pieces
+
+  def initialize(position, color, board)
+    super(position, color, board)
+    @moved = false
+    set_moves
+  end
+
+  attr_accessor :pawn_motion
+
+  def set_moves
+    if color == :white
+      @pawn_motion = [[0,1],[1,1],[-1,1],[0,2]]
+    else
+      @pawn_motion = [[0,-1],[1,-1],[-1,-1],[0,-2]]
+    end
+
+  end
+
+  def pawn_moves
+    pos=[]
+    if @moved
+      @pawn_motion = pawn_motion.take(3)
+    end
+
+    pawn_motion.each do |move|
+      pos << [position.first + move.first, position.last + move.last]
+    end
+
+    remove_invalid(pos)
+  end
+
+
 
 
 end
