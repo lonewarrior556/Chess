f3e398bcd08e54207c269a28469779aad7a89d59
Alex & Konrad <Alex_Konrad@appacademy.io>
2015-03-31 14:45:18 -0400

    createboard with piece objects, create hash to translate human input

--- board.rb
+++ board.rb
@@ -2,7 +2,9 @@ require './piece'
 
 class Board
 
-  attr_accessor :board
+
+
+  attr_accessor :board, :dictionary
 
   ORDER= [:rook, nil, :bishop, :queen, nil, :bishop, nil, :rook]
   K_UNITS = [nil, :knight, nil, nil, :king, nil,:knight, nil]
@@ -24,6 +26,19 @@ class Board
       @board[7][col] = Stepping_Pieces.new([7,col],:black,[],K_UNITS[col])
     end
 
+  end
+
+  def create_board_hash
+    @dictionary = Hash.new
+    columns = %w(a b c d e f g h)
+
+    @board.each_with_index do |row, row_idx|
+      row.each_with_index do |col, col_idx|
+        dictionary[columns[col_idx] + (8 - row_idx).to_s] = [row_idx,col_idx]
+      end
+    end
+
+    nil
   end
 
 
